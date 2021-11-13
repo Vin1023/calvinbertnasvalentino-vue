8943e8f4efcecf02df622f7c063e02a3471d0381
Vin1023 <93768534+Vin1023@users.noreply.github.com>
2021-11-12 20:43:24 -0800

    Portofolio

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 <div id ="app">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <img alt ="Vue logo" src="./assets/portofolio.png" width="1519">
+ 
 <Logos /></div>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,51 @@
 <template>
  <div class="hello">
-   
-   
+  <div class="jumbotron text-center">
+    
+  <h1 style="color:wheat">Portofolio</h1>
+  
+</div>
+
+<div class="container">
+  <div class="row">
+    
+    <div class="col-sm-4">
+      <h3 style="color:wheat">Data Diri</h3>
+      <p style="color:wheat">Nama : Calvin Bertnas Valentino</p>
+      <p style="color:wheat">Tempat,Tanggal Lahir : Palembang,10 Februari 2003</p>
+      <p style="color:wheat">Agama : Buddha</p>
+      <p style="color:wheat">Pekerjaan : Supir</p>
+    </div>
+    <div class="col-sm-4">
+      <h3 style="color:wheat">Hobi</h3>
+      <p style="color:wheat">-Menonton Film</p>
+      <p style="color:wheat">-Main Rubik</p>
+      <p style="color:wheat">-Main Game</p>
+      <p style="color:wheat">-Olahraga</p>
+      
+    </div>
+    <div class="col-sm-4">
+      <h3 style="color:wheat">Pendidikan</h3>
+      <p style="color:wheat">-TK Anisa</p>
+      <p style="color:wheat">-SD Bina Bangsa</p>
+      <p style="color:wheat">-SD Methodist 2</p>
+      <p style="color:wheat">-SMP Methodist 2</p>
+      <p style="color:wheat">-SMA Methodist 2</p>
+    </div>
+    <div class="col-sm-4">
+      <h3 style="color:wheat">Karakter</h3>
+      <p style="color:wheat">-Baik</p>
+      <p style="color:wheat">-Sopan</p>
+      <p style="color:wheat">-Jujur</p>
+      <p style="color:wheat">-Disiplin</p>
+      <p style="color:wheat">-Bertanggung Jawab</p>
+     
+    </div>
   </div>
+</div>
+
+  </div>
+  
    </template>
 
 <script>
